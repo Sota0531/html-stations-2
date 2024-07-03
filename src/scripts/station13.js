95da5d2c6a288923fcd1ca0351c4eced2de6af54
Sota0531 <na.3105.so@icloud.com>
2024-07-03 17:42:33 +0900

    S13.fin

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,9 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
-
+  var s = document.getElementById("check").checked;
+  if(s){
+    document.getElementById("text").style.backgroundColor = "red";
+  }else{
+    document.getElementById("text").style.backgroundColor = "transparent";
+  }
 }
